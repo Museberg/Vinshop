195e2337f3c409502cfcda37e1457302e84f05e9
Museberg <Rasmus@falk-jensen.dk>
2020-11-25 10:22:30 +0100

    Initial commit

---
+++ src/main/java/com/egfds/vinshop/VinshopApplication.java
@@ -0,0 +1,13 @@
+package com.egfds.vinshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VinshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VinshopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/egfds/vinshop/VinshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.egfds.vinshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VinshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
