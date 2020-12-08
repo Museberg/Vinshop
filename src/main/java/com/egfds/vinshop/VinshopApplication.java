53b2ad1c71f7dc10ba27b912704b209a64883063
MaybeMike1 <57484151+MaybeMike1@users.noreply.github.com>
2020-12-08 13:31:44 +0000

    Sign-up form is implemented ready for review.

--- src/main/java/com/egfds/vinshop/VinshopApplication.java
+++ src/main/java/com/egfds/vinshop/VinshopApplication.java
@@ -1,12 +1,13 @@
 package com.egfds.vinshop;
 
+import com.egfds.vinshop.repositories.ProductRepos.IProductTypeRepo;
 import com.egfds.vinshop.repositories.UserRepos.IUserRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories(basePackageClasses = IUserRepository.class)
+@EnableJpaRepositories(basePackageClasses = {IUserRepository.class, IProductTypeRepo.class})
 public class VinshopApplication {
 
 
