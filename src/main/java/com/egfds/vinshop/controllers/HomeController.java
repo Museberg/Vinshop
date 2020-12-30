9d7f85ca9eda6baee4754fc8615fe9acadb70d7d
Museberg <Rasmus@falk-jensen.dk>
2020-12-30 12:15:35 +0100

    Fixed merge issue and fixed cart check in navbar

--- src/main/java/com/egfds/vinshop/controllers/HomeController.java
+++ src/main/java/com/egfds/vinshop/controllers/HomeController.java
@@ -13,6 +13,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.Arrays;
